ee98ab04298200822c3ad2e21be8d23f7b56b363
LoganLilypad <40219242+LoganLilypad@users.noreply.github.com>
2019-02-27 15:47:36 -0500

    Made !help command a bit prettier

--- bot.py
+++ bot.py
@@ -27,13 +27,14 @@ async def on_message(message):
         return
     if message.content.startswith('!help'):
         embed = discord.Embed(title="Help", description="List of the available commands:\n", color=0x609dff)
-        embed.add_field(name="!help", value="Shows this menu", inline=True)
-        embed.add_field(name="!q <type>", value="Asks a random question of the said category", inline=True)
-        embed.add_field(name="!a <answer>", value="Answer a question (case does not matter)", inline=True)
-        embed.add_field(name="!gif <query>", value="Search for a gif from Giphy", inline=True)
-        embed.add_field(name="!img <query>", value="Search for an image from Imgur", inline=True)
-        embed.add_field(name="!del <message ID>", value="Deletes a message by ID", inline=True)
-        embed.add_field(name="!contribute", value="Wanna help with this bot? Run this command", inline=True)
+        embed.add_field(name="!help", value="Shows this menu", inline=False)
+        embed.add_field(name="!role <name> <session (1/3)>", value="Setup your name and session so you can talk in the Discord", inline=False)
+        embed.add_field(name="!q <type>", value="Asks a random question of the said category", inline=False)
+        embed.add_field(name="!a <answer>", value="Answer a question (case does not matter)", inline=False)
+        embed.add_field(name="!gif <query>", value="Search for a gif from Giphy", inline=False)
+        embed.add_field(name="!img <query>", value="Search for an image from Imgur", inline=False)
+        embed.add_field(name="!del <message ID>", value="Deletes a message by ID", inline=False)
+        embed.add_field(name="!contribute", value="Wanna help with this bot? Run this command", inline=False)
         await message.channel.send(embed=embed)
     elif message.content.startswith("!del"):
         msg = message.content.replace("!del", "")
