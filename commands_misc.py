323edb7de85df412cff48fb31984b24719bbe6cf
LoganLilypad <40219242+LoganLilypad@users.noreply.github.com>
2019-04-15 17:02:37 -0400

    Update commands_misc.py

--- commands_misc.py
+++ commands_misc.py
@@ -1,14 +1,19 @@
 import discord
 import requests
 
+def sendError(m):
+    embed = discord.Embed(title="Error", description=m, color=0xfc4444)
+    return embed
+
 def commands_role(msg):
     msg = msg.replace("!role", "")
     if msg == "":
-        await return(sendError("Usage: `!role <name> <session (1/3)>`"))
+        return sendError("Usage: `!role <name> <session (1/3)>`")
     else:
+        #Really need to either delete this, or actually find how to assign roles/nicknames using Discord Rewrite; directly using the API is ugly
         msg = msg.split(" ")
         if len(msg) < 2:
-            await message.channel.send(emed=sendError("Usage: `!role <name> <session (1/3)>`"))
+            return emed=sendError("Usage: `!role <name> <session (1/3)>`")
         elif msg[2] == "1" or msg[2] == "3":
             r = requests.patch("https://discordapp.com/api/v6/guilds/%s/members/%s" %(message.guild.id, message.author.id), headers={"Authorization":"Bot %s" %TOKEN,"Content-Type":"application/json"}, data=json.dumps({"nick":msg[1]}))
             if msg[2] == "1":
@@ -17,12 +22,12 @@ def commands_role(msg):
                 r2 = requests.patch("https://discordapp.com/api/v6/guilds/%s/members/%s" %(message.guild.id, message.author.id), headers={"Authorization":"Bot %s" %TOKEN,"Content-Type":"application/json"}, data=json.dumps({"roles":["522884365177585686"]}))
             if "Missing Permissions" in r.text:
                 embed = discord.Embed(title=":x: Looks like I don't have permission to change your nickname :shrug:", description="", color=0xfc4444)
-                await message.channel.send(embed=embed)
+                return embed
             if "Missing Permissions" in r2.text:
                 embed = discord.Embed(title=":x: Looks like I don't have permission to change your role :shrug:", description="", color=0xfc4444)
-                await message.channel.send(embed=embed)
+                return embed
             else:
                 embed = discord.Embed(title="<:correct:548988506496696341> Success!", description="", color=0x56ff67)
-                await message.channel.send(embed=embed)
+                return embed
         else:
-            await message.channel.send(emed=sendError("Valid sessions are 1 and 3"))
+            return sendError("Valid sessions are 1 and 3")
