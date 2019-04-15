13d40346210d35e34c5e959c2e6602ec21961ee6
LoganLilypad <40219242+LoganLilypad@users.noreply.github.com>
2019-04-15 16:50:38 -0400

    Update commands_it.py

--- commands_it.py
+++ commands_it.py
@@ -1,1 +1,13 @@
+import discord
 
+def command_help():
+    embed = discord.Embed(title="Help", description="List of the available commands:\n", color=0x609dff)
+    embed.add_field(name="!help", value="Shows this menu", inline=False)
+    embed.add_field(name="!role <name> <session (1/3)>", value="Setup your name and session so you can talk in the Discord", inline=False)
+    embed.add_field(name="!q <type>", value="Asks a random question of the said category", inline=False)
+    embed.add_field(name="!a <answer>", value="Answer a question (case does not matter)", inline=False)
+    embed.add_field(name="!gif <query>", value="Search for a gif from Giphy", inline=False)
+    embed.add_field(name="!img <query>", value="Search for an image from Imgur", inline=False)
+    embed.add_field(name="!del <message ID>", value="Deletes a message by ID", inline=False)
+    embed.add_field(name="!contribute", value="Wanna help with this bot? Run this command", inline=False)
+    return embed
